0eead324f6bb121210d7ad718b0886aa47033838
tornike2002 <toko.toka2000@gmail.com>
2024-07-19 23:04:36 +0400

    removed unnecesery stuff

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,10 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className=" ">
-          <Navbar />
-          {children}
-        </div>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
